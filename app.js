61f06e33bce94e07b1f4f35625f0433253cd04bc
luk1pr0 <lpytel16@gmail.com>
2021-03-21 14:09:44 +0000

    Added styling to the burger menu when nav is open and also styled the crossbar

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ const toggleNav = () => {
 	// Toggle hidden class
 	nav.classList.toggle('nav--hidden');
 
+	// Change the burger lines to cross each other when nav is open
+	burger.classList.toggle('burger--cross');
+
 	// If nav is shown then hide the scroll bar
 	if (!nav.className.includes('nav--hidden')) {
 		document.body.style.overflowY = 'hidden';
+
+		// Add margin as the width of the scrollbar
+		burger.style.marginRight = '10px';
 	} else {
 		document.body.style.overflowY = 'auto';
+		burger.style.marginRight = '0px';
 	}
 }
 
