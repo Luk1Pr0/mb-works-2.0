ba20a5723185fbbbdb92f6ddd26279f6489abe16
Luk1Pr0 <lpytel16@gmail.com>
2021-03-22 15:47:35 +0000

    Added on load animation to the header and navigation elements

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const navContainer = document.getElementById('nav-container');
+
 const nav = document.getElementById('navigation');
 const burger = document.getElementById('burger');
 
-const slogan = document.querySelectorAll('.slide');
+const sloganText = document.querySelectorAll('.animate-slide');
+const dividerLine = document.querySelector('.divider-line');
 
 // Toggle navigation
 const toggleNav = () => {
@@ -28,5 +31,28 @@ const closeNav = () => {
 	nav.classList.add('nav--hidden');
 }
 
+// Stretch the divider line of the slogan
+const stretch = () => {
+	dividerLine.classList.add('stretch');
+}
+
+// Slide elements
+const slideRight = () => {
+	sloganText.forEach((text, i) => {
+		text.style.transitionDelay = `${i / 2}s`
+		text.classList.add('animate-slide--active');
+	});
+}
+
+// Show nav logo and burger menu
+const slideDown = () => {
+	navContainer.classList.add('nav--show');
+}
+
 // Event listeners
 burger.addEventListener('click', toggleNav);
+
+// On load
+slideDown();
+stretch();
+setTimeout(slideRight, 500);
